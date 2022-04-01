5b604db835f07d390f5029e7e10e8ff232fdfd0a
BDSand <86014683+BDSand@users.noreply.github.com>
2022-03-31 23:31:38 -0500

    netflixy

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "18bcb8e67a1642daefd44f3382ac5f36",
+    authDomain: "netflix-clone-9f83e.firebaseapp.com",
+    projectId: "netflix-clone-9f83e",
+    storageBucket: "netflix-clone-9f83e.appspot.com",
+    messagingSenderId: "339454087158",
+    appId: "1:339454087158:web:63a9c479f14964634b41d7",
+    measurementId: "G-ZEH7WFGFJC"
+  };
